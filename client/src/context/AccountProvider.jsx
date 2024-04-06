4b49a55774e196a1cfbf1c42772314d2f3d157e9
safar123456 <abhiav498@gamil.com>
2024-04-07 01:17:00 +0530

    new build

--- client/src/context/AccountProvider.jsx
+++ client/src/context/AccountProvider.jsx
@@ -16,7 +16,7 @@ const AccountProvider = ({children}) => {
     const socket = useRef();
 
     useEffect(() => {
-        socket.current = io('ws://localhost:9000');
+        socket.current = io('ws://abhinav.eastus.cloudapp.azure.com:9000');
     }, [])
 
     return (
